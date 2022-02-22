96691afed6c14cd003612b1fd74b64a556e8c53c
ciocardagelu@gmail.com <ciocardagelu@gmail.com>
2022-02-22 23:28:20 +0200

    m_listaConturi after an user deletion is reflected in .csv file

--- ProiectBancaOOP/ManagerConturi.h
+++ ProiectBancaOOP/ManagerConturi.h
@@ -7,7 +7,7 @@ class ManagerConturi
 {
 private:
 	std::vector<ContBancar*> m_listaConturi;
-	//TODO: metoda createIban trebuie sa genereze un Iban unic care sa fie folosit 
+	//TODO-done: metoda createIban trebuie sa genereze un Iban unic care sa fie folosit 
 	//pentru a crea conturi
 	std::string CreateIban();//TODO: BUG: Iban is not entirely unique brecause if we stop the program it starts reusing keys
 	ContBancar* FindAccount();
